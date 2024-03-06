a008f72333aa83902064e03727e9eee5fce291e2
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-06 15:35:13 +0700

    Update Data_Preparation.sql

--- Data_Analysis_with_SQL/Data_Preparation.sql
+++ Data_Analysis_with_SQL/Data_Preparation.sql
@@ -6,7 +6,24 @@
 
 -- 1.2. Binning 
 -- Arbitrary-sized Bin (CASE WHEN)
-
+WITH total AS (
+	SELECT	order_id,
+			SUM(quantity) AS total_demand,
+			CASE
+				WHEN SUM(list_price) <= 2000 THEN '0 - 2000'
+				WHEN SUM(list_price) <= 4000 THEN '2000 - 4000'
+				WHEN SUM(list_price) <= 6000 THEN '4000 - 6000'
+				WHEN SUM(list_price) <= 8000 THEN '6000 - 8000'
+				WHEN SUM(list_price) <= 10000 THEN '8000 - 10000'	
+				ELSE 'More than 10000' END AS payment_range
+	FROM	sales.order_items
+	GROUP BY order_id
+	)
+SELECT	payment_range,
+		COUNT(order_id) AS num_of_orders 
+FROM	total
+GROUP BY payment_range
+ORDER BY payment_range ASC;
 
 -- Fixed Size
 -- ROUND()
@@ -27,3 +44,5 @@ FROM	(
 		GROUP BY ord.customer_id, ord.order_id) bin_range
 GROUP BY bins 
 ORDER BY 1;
+
+-- PERCENT_TANK() OVER (PARTITION BY field_name ORDER BY field_name) 
