4e65bb06c51d52cc9a490dd6f44f555c3ad270eb
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-06 09:53:14 +0700

    Update and rename Data_Analysis_with_SQL.sql to Data_Analysis_with_SQL/Data_Preparation.sql

---
+++ Data_Analysis_with_SQL/Data_Preparation.sql
@@ -0,0 +1,16 @@
+-- Profiling Data
+-- 1. Distributions
+
+-- 1.1. Histograms // Frequencies 
+-- (GROUP BY)
+
+-- 1.2. Binning 
+-- Arbitrary-sized Bin (CASE WHEN)
+
+
+-- Fixed Size
+-- ROUND()
+
+-- LOG()
+
+-- NTILE()
