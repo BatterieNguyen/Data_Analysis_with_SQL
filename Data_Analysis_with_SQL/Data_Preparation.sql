e70496194b5c415139becee9ffa2c412e4cb959d
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-06 10:28:31 +0700

    Update Data_Preparation.sql

--- Data_Analysis_with_SQL/Data_Preparation.sql
+++ Data_Analysis_with_SQL/Data_Preparation.sql
@@ -15,8 +15,8 @@
 
 -- NTILE(num_of_bins) OVER (PARTITION BY field_name  ORDER BY  field_name)
 SELECT	ord.customer_id, ord.order_id,
-		NTILE(10) OVER (ORDER BY SUM(ite.list_price)) AS bins
+	NTILE(10) OVER (ORDER BY SUM(ite.list_price)) AS bins
 FROM	sales.orders ord
-		INNER JOIN	sales.order_items ite ON ord.order_id = ite.order_id
+	INNER JOIN	sales.order_items ite ON ord.order_id = ite.order_id
 GROUP BY ord.customer_id, ord.order_id
 ORDER BY 1;
