1187b62d28439dc3d6070687328f8b9fe47454c5
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-09 10:28:43 +0700

    Update Data_Preparation.sql

--- Data_Analysis_with_SQL/Data_Preparation.sql
+++ Data_Analysis_with_SQL/Data_Preparation.sql
@@ -46,7 +46,78 @@ GROUP BY bins
 ORDER BY 1;
 
 -- PERCENT_TANK() OVER (PARTITION BY field_name ORDER BY field_name) 
+SELECT	order_id,
+		PERCENT_RANK() OVER (ORDER BY list_price) AS percentile
+FROM	sales.order_items;
 
+SELECT * FROM sales.order_items;
+
+--------------------------------------------------
 -- 2. Detecting Duplication
+-- 2.1. GROUP BY // HAVING
+
+SELECT	records, COUNT(*)
+FROM	(
+		SELECT	product_id, product_name, brand_id, category_id, model_year, list_price,
+				COUNT(*) AS records
+		FROM	production.products
+		GROUP BY product_id, product_name, brand_id, category_id, model_year, list_price
+		) a
+GROUP BY	records;
+
+
+-- 2.2. JOIN and DISTINCT
+
+--------------------------------------------------
+-- 3. Data Cleaning
+-- 3.1. CASE WHEN 
+-- Label
+-- Order status: 1 = Pending; 2 = Processing; 3 = Rejected; 4 = Completed
+
+SELECT	order_status,
+		COUNT(order_id) AS orders
+FROM	(
+		SELECT	order_id,
+				CASE
+					WHEN order_status = 1 THEN 'Pending'
+					WHEN order_status = 2 THEN 'Processing'
+					WHEN order_status = 3 THEN 'Rejected'
+					ELSE 'Completed'
+				END AS order_status
+		FROM	sales.orders
+		) a
+GROUP BY order_status 
+ORDER BY 1 ASC;
+
+-- Categorization (string // int -> string // nested) 
+
+-- Dummy Variables
+
+-- 3.2. Type Conversion and Casting
+-- 3.2.1. TO_VARCHAR // TO_NUMBER // TO_DATE // TO_TIMESTAMP
+
+
+-- 3.2.2. REPLACE()	-- REPLACE(column_name, 'search_string', 'replacement_string')::FLOAT
+			-- CONVERT(FLOAT, REPLACE(column_name, 'search_string', 'replacement_string'))
+			-- CAST(REPLACE(column_name, 'search_string', 'replacement_string') AS FLOAT)
+
+-- 3.2.3. CAST()
+SELECT 	CAST(column_name AS FLOAT) AS modified_column
+	FROM your_table;
+
+-- 3.2.4. DATE()
+SELECT 	CONVERT(DATE,CONCAT(year, '-', month, '-', day)) AS converted_date
+FROM 	your_table;
+
+-- 3.3. Dealing with Nulls
+-- 3.3.1. CASE WHEN 
+
+-- 3.3.2. COALESCE()
+
+-- 3.3.3. NULLIF()
+-- change it back into the null value
+
+
+
 
 
