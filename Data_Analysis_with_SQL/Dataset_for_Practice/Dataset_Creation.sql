46dfaadf53ac3b2006e87536c24a9991702f1f86
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-15 11:59:47 +0700

    Update Dataset_Creation.sql

--- Data_Analysis_with_SQL/Dataset_for_Practice/Dataset_Creation.sql
+++ Data_Analysis_with_SQL/Dataset_for_Practice/Dataset_Creation.sql
@@ -3,6 +3,34 @@ USE practices;
 /****************************************************************************************/
 -- US RETAIL SALES DATASET
 
+DROP TABLE IF EXISTS us_retail_sales;
+
+-- Create table
+CREATE TABLE us_retail_sales	
+      (
+      	sales_month DATE,
+      	naics_code VARCHAR(255),
+      	kind_of_business VARCHAR(255),
+      	reason_for_null VARCHAR(255),
+      	sales DECIMAL (10, 2)
+      );
+
+/*
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+*/
+
+-- Import data from CSV file
+BULK INSERT	us_retail_sales
+FROM 'D:/0. Let''s Stress/DATA ANALYSIS/SQL/SQLServer/us_retail_sales.csv'
+WITH (
+  	  FORMAT = 'CSV',
+      FIELDTERMINATOR = ',',
+      ROWTERMINATOR = '0x0a',
+      FIRSTROW = 2
+      );
 
 /****************************************************************************************/
 -- LEGISLATORS DATASET
