74d71b7e1e36b3ca15a7a08d6b567ba06726118d
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-23 00:07:35 +0700

    Update and rename Sorting_Query_Result.md to Sorting_Query_Result.sql

---
+++ SQL_Query_from_pieces/Sorting_Query_Result.sql
@@ -0,0 +1,11 @@
+---- SORTING BY SUBSTRING
+
+-- sort value by the last 2 characters in the job field
+-- DB2 | MySQL | Oracle | PostgreSQL
+ORDER BY  SUBSTR(job, length(job) -1)
+
+-- SQL Server
+ORDER BY  SUBSTRING(job, len(job) -1, 2)
+
+
+-- Sorting mixed alphanumeric data
