61104fa801720a9ef1baa8ec03021fc5cf07d788
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2024-03-23 01:25:26 +0700

    Update Sorting_Query_Result.sql

--- SQL_Query_from_pieces/Sorting_Query_Result.sql
+++ SQL_Query_from_pieces/Sorting_Query_Result.sql
@@ -1,11 +1,18 @@
 ---- SORTING BY SUBSTRING
 
--- sort value by the last 2 characters in the job field
--- DB2 | MySQL | Oracle | PostgreSQL
+-- 1.1. Sort value by the last 2 characters in the job field
+  -- DB2 | MySQL | Oracle | PostgreSQL
 ORDER BY  SUBSTR(job, length(job) -1)
 
--- SQL Server
+  -- SQL Server
 ORDER BY  SUBSTRING(job, len(job) -1, 2)
 
 
--- Sorting mixed alphanumeric data
+-- 1.2. Sorting mixed alphanumeric data
+
+CREATE VIEW V	AS
+	SELECT CONCAT(ename, ' ', deptno) AS data
+	FROM	emp
+
+	SELECT * FROM V;
+
